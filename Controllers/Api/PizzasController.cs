2d1e8ed0387c61aa2bae4717a07267ef653dc79a
Simone Mauro <simon.mauro@virgilio.it>
2022-07-26 14:50:52 +0200

    Apicontroller e inizio chiamata axios

---
+++ Controllers/Api/PizzasController.cs
@@ -0,0 +1,22 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers.Api{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzasController : ControllerBase{
+
+        [HttpGet]
+        public IActionResult Get(){
+            using(PizzaContext context = new PizzaContext()){
+
+                IQueryable<Pizza> pizze = context.Pizzas;
+                
+                return(Ok(pizze.ToList()));
+            }
+        }
+    }
+}
